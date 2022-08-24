4d803973be6fc265c2fad56f3e0b522d291c464a
Antonela Borgogno <antonelaborgogno@gmail.com>
2022-08-23 23:43:45 -0300

    retoques finales

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  Api:'https://backend-ap-anto.herokuapp.com',
-  // Api: 'http://localhost:8080',
+  // Api:'https://backend-ap-anto.herokuapp.com',
+  Api: 'http://localhost:8080',
   isLogged: true
 };
